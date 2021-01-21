c81bc45f051c3e40eda583d8411cfcaf99cf74f1
JoseLRiano <joselriano@yahoo.com>
2021-01-20 19:09:33 -0500

    Adding packages and plugins for Gatsby Image

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,20 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require(`path`);
+
 module.exports = {
   /* Your site config here */
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'images',
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
